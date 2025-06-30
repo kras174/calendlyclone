42be31c4bff9234e29192268d9aa99ba051be4b3
kras174 <kras32a@gmail.com>
2025-06-30 12:05:31 +0500

    navbar added

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import PrivateNavBar from '@/components/PrivateNavBar';
+import PublicNavBar from '@/components/PublicNavBar';
+import React from 'react';
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function MainLayout({ children }: { children: React.ReactNode }) {
+  const user = await currentUser();
+
+  return (
+    <main className="relative">
+      {user ? <PrivateNavBar /> : <PublicNavBar />}
+      <section className="pt-36">{children}</section>
+    </main>
+  );
+}
