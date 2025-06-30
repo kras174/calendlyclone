4878290af05f690045ee94b3a2ad5b52a0c02fa1
kras174 <kras32a@gmail.com>
2025-06-30 13:28:55 +0500

    get events from db

--- app/(main)/(private)/events/page.tsx
+++ app/(main)/(private)/events/page.tsx
@@ -3,17 +3,15 @@ import { CalendarPlus, CalendarRange } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { auth } from '@clerk/nextjs/server';
+import { getEvents } from '@/server/actions/events';
 
 // import EventCard from '@/components/cards/EventCard';
-
-// import { getEvents } from '@/server/actions/events';
-
 export default async function EventsPage() {
   const { userId, redirectToSignIn } = await auth();
 
   if (!userId) return redirectToSignIn();
 
-  // const events = await getEvents(userId);
+  const events = await getEvents(userId);
 
   return (
     <section className="flex flex-col items-center gap-16 animate-fade-in">
@@ -31,26 +29,27 @@ export default async function EventsPage() {
       </div>
 
       {/* Show event cards if any exist, otherwise show empty state */}
-      {/* {events.length > 0 ? (
+      {events.length > 0 ? (
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-10">
           {events.map((event) => (
-            <EventCard key={event.id} {...event} />
+            <div>{event.name}</div>
+            // <EventCard key={event.id} {...event} />
           ))}
         </div>
-      ) : ( */}
-      <div className="flex flex-col items-center gap-4">
-        <CalendarRange className="size-16 mx-auto text-black" />
-        You do not have any events yet. Create your first event to get started!
-        <Button
-          className="bg-blue-500 hover:bg-blue-400 text-white py-6 hover:scale-110 duration-500 border-b-4 border-blue-700 hover:border-blue-500 rounded-2xl shadow-accent-foreground shadow-2xl text-2xl font-black"
-          asChild
-        >
-          <Link href="/events/new">
-            <CalendarPlus className="mr-4 size-7" /> New Event
-          </Link>
-        </Button>
-      </div>
-      {/* )} */}
+      ) : (
+        <div className="flex flex-col items-center gap-4">
+          <CalendarRange className="size-16 mx-auto text-black" />
+          You do not have any events yet. Create your first event to get started!
+          <Button
+            className="bg-blue-500 hover:bg-blue-400 text-white py-6 hover:scale-110 duration-500 border-b-4 border-blue-700 hover:border-blue-500 rounded-2xl shadow-accent-foreground shadow-2xl text-2xl font-black"
+            asChild
+          >
+            <Link href="/events/new">
+              <CalendarPlus className="mr-4 size-7" /> New Event
+            </Link>
+          </Button>
+        </div>
+      )}
     </section>
   );
 }
