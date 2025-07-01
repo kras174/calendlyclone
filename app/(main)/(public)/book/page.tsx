a81190a2e987614fc53e67b5c6d116cd500e55f0
kras174 <kras32a@gmail.com>
2025-07-01 16:19:43 +0500

    add public profile component

--- app/(main)/(public)/book/page.tsx
+++ app/(main)/(public)/book/page.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+'use client';
 
-export default function Page() {
-  return <h1>Book page</h1>;
+import Loading from '@/components/Loading';
+import { redirect } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
+
+export default function PublicPage() {
+  const { user, isLoaded } = useUser(); // Using `isLoaded` to check if user data is available
+
+  if (!isLoaded) {
+    // Display loading until user data is loaded
+    return <Loading />;
+  }
+
+  if (!user) {
+    // Redirect to login if no user is found
+    return redirect('/login');
+  }
+
+  // Once user is available, redirect to the booking page [Public Profile Page]
+  return redirect(`/book/${user.id}`);
 }
